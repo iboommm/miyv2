28f50379854c9cd1c6f18ebd621195df0cc9085b
Wuthiwat Sornso <iboommm@live.com>
2017-04-05 16:37:06 +0700

    add login page

---
+++ views/login/login.js
@@ -0,0 +1,4 @@
+angular.module('miyv2').controller('LoginController',['$scope','$state','$http','homeService',function($scope,$state,$http,homeService) {
+
+
+}]);
---
+++ views/login/login.service.js
@@ -0,0 +1,27 @@
+angular.module('miyv2').service('loginService',['$http','$q',function($http,$q){
+    var self = this;
+
+
+    var deferObject,serviceMethods = {
+        //TODO your code here
+        getToken : function(){
+           var params = {page: "admin"};
+            var promise = $http.get("api/token/get",params),
+                deferObject = deferObject || $q.defer();
+
+            promise.then(
+                function(answer){
+                    deferObject.resolve(answer);
+                },
+                function(reason){
+                    deferObject.reject(reason);
+                }
+            );
+            return deferObject.promise;
+
+        },
+
+
+    };
+    return serviceMethods;
+    }]);
